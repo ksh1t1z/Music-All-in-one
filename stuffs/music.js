8f93e25c355f1897ba3df55f540aa64d38df1812
ksh1t1z <49852633+ksh1t1z@users.noreply.github.com>
2020-03-22 11:23:57 +0545

    Add files via upload

---
+++ stuffs/music.js
@@ -0,0 +1,31 @@
+function mainUpdate(type) {
+    if (type == "music") {
+        document.getElementById('artwork').src = "/var/mobile/Documents/Artwork.jpg?" + (new Date()).getTime();
+        document.getElementById('title').innerHTML = title;
+        document.getElementById('artist').innerHTML = artist;
+
+        if (isplaying) {
+            document.getElementById('player').style.visibility = "visible";
+            document.getElementById('artwork').style.visibility = "visible";
+            document.getElementById('previous').style.visibility = "visible";
+            document.getElementById('next').style.visibility = "visible";
+            document.getElementById('pP').src = "img/pause.png";
+        } else {
+            document.getElementById('player').style.visibility = "hidden";
+            document.getElementById('artwork').style.visibility = "hidden";
+            document.getElementById('previous').style.visibility = "hidden";
+            document.getElementById('next').style.visibility = "hidden";
+            document.getElementById('pP').src = "img/play.png";
+        }
+    }
+}
+
+function previous() {
+    window.location = 'xeninfo:prevtrack'
+}
+function pP() {
+    window.location = 'xeninfo:playpause'
+}
+function next() {
+    window.location = 'xeninfo:nexttrack'
+}
